9b09eba5f30aec8f96083dacc1fee27f3eaa5b6e
karripartanen <karripartanen25@gmail.com>
2025-09-21 16:33:27 +0300

    testing github workflow again

--- content-server/src/api/index.ts
+++ content-server/src/api/index.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from 'express';
-
+import testRoute from './routes/testRoute';
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
@@ -9,6 +9,6 @@ router.get('/', (req: Request, res: Response) => {
 }
 );
 
-// router.use('/favorites', favoritesRouter); // Example of how to add more routes in the future
+router.use('/test', testRoute);
 
 export default router;
