4ce1b92641983c9d3ff796ec224aa96ab536b505
ellinor <ellinoora.riskala@metropolia.fi>
2025-11-10 21:14:09 +0200

    instruction steps toggle

--- content-server/src/api/index.ts
+++ content-server/src/api/index.ts
@@ -2,19 +2,22 @@ import express, {Request, Response} from 'express';
 import testRoute from './routes/dataRoute';
 import dataRoute from './routes/dataRoute';
 import contactRoute from './routes/contactRoute';
+import instructionRoute from './routes/instructionRoute';
+
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
   res.json({
-    message: 'Routes in use: data',
-  })
-}
-);
+    message: 'Routes in use: data, contact, instructions',
+  });
+});
 
 router.use('/test', testRoute);
 
 router.use('/data', dataRoute);
 
 router.use('/contact', contactRoute);
+
+router.use('/instructions', instructionRoute);
 
 export default router;
