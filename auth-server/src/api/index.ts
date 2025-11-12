c0f6c3a00f1543ec9031c6447d23a87f3509835e
karripartanen <karripartanen25@gmail.com>
2025-11-13 00:09:46 +0200

    fixed a route removed by someone

--- auth-server/src/api/index.ts
+++ auth-server/src/api/index.ts
@@ -3,24 +3,23 @@ import authRoute from './routes/authRoute';
 import userRoute from './routes/userRoute';
 import profileRoute from './routes/profileRoute';
 import tipsRoute from './routes/tipsRoute';
-
+import adminRoute from './routes/adminRoute';
 
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
   res.json({
     message: 'Routes in use: /api/profile, /api/tips',
-  })
-}
-);
+  });
+});
 router.get<{}, {message: string}>('/ping', (_req: Request, res) => {
   res.json({message: 'pong'});
 });
-
 
 router.use('/auth', authRoute);
 router.use('/users', userRoute);
 router.use('/profile', profileRoute);
 router.use('/tips', tipsRoute);
+router.use('/admin', adminRoute);
 
 export default router;
