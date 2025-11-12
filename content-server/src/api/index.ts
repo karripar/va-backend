58adcfc2d30c84c040285a4484c1d35e5cc6c098
unknown <uweraodette01@gmail.com>
2025-11-12 22:36:32 +0200

    Fix route imports and enable addDocument endpoint + all tests passing

--- content-server/src/api/index.ts
+++ content-server/src/api/index.ts
@@ -1,16 +1,18 @@
 import express, {Request, Response} from 'express';
-import testRoute from './routes/dataRoute';
-import dataRoute from './routes/dataRoute';
+import destinationRoute from './routes/destinationRoute';
+import contactRoute from './routes/contactRoute';
+import instructionRoute from './routes/instructionRoute';
+
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
   res.json({
-    message: 'Routes in use: nothing yet',
-  })
-}
-);
+    message: 'Routes in use: destinations, contact, instructions',
+  });
+});
 
-router.use('/test', testRoute);
-router.use('/data', dataRoute);
+router.use('/destinations', destinationRoute);
+router.use('/contact', contactRoute);
+router.use('/instructions', instructionRoute);
 
 export default router;
