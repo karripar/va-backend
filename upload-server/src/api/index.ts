d3bf86233609294e67cc0c3b51569d24325b20a0
ellinor <ellinoora.riskala@metropolia.fi>
2025-11-14 13:05:10 +0200

    fix

--- upload-server/src/api/index.ts
+++ upload-server/src/api/index.ts
@@ -1,16 +1,17 @@
 import express, {Request, Response} from 'express';
 import testRoute from './routes/testRoute';
 import storyPhotoRoute from './routes/storyPhotoRoute';
+import fileRoute from './routes/fileRoute';
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
   res.json({
-    message: 'Routes in use: /test, /upload',
-  })
-}
-);
+    message: 'Routes in use: /test, upload, delete, list',
+  });
+});
 
 router.use('/test', testRoute);
 router.use('/upload', storyPhotoRoute);
+router.use('/uploads', fileRoute);
 
 export default router;
